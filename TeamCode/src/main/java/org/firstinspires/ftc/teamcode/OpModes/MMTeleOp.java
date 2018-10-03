c90ce3f55c42037951cd2e550ce5b0e7c182e4b8
Eric Seng <eseng2021@gmail.com>
2018-10-02 19:24:16 -0500

    pushed more code for richik to look at

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/MMTeleOp.java
@@ -0,0 +1,18 @@
+package main.java.org.firstinspires.ftc.teamcode.OpModes;
+import main.java.org.firstinspires.ftc.teamcode.*;
+import com.edinaftc.ninevolt.Ninevolt;
+import com.edinaftc.ninevolt.core.hw.drivetrain.MecanumMovement;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.edinaftc.ninevolt.core.hw.Hardware;
+import com.edinaftc.ninevolt.core.hw.HardwareBuilder;
+import com.edinaftc.ninevolt.core.hw.drivetrain.Movement;
+
+public class MMTeleOp extends OpMode {
+
+}
