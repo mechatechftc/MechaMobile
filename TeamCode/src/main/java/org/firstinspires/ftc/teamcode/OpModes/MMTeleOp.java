11c1497459f29c7be646d9ed04a5f0aa195359bc
Eric Seng <eseng2021@gmail.com>
2018-10-02 19:45:17 -0500

    push code for richik v.2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/MMTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/MMTeleOp.java
@@ -1,18 +1,7 @@
-package main.java.org.firstinspires.ftc.teamcode.OpModes;
-import main.java.org.firstinspires.ftc.teamcode.*;
-import com.edinaftc.ninevolt.Ninevolt;
-import com.edinaftc.ninevolt.core.hw.drivetrain.MecanumMovement;
-import com.qualcomm.hardware.bosch.BNO055IMU;
-import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.edinaftc.ninevolt.core.hw.Hardware;
-import com.edinaftc.ninevolt.core.hw.HardwareBuilder;
-import com.edinaftc.ninevolt.core.hw.drivetrain.Movement;
+package org.firstinspires.ftc.teamcode.OpModes;
+import org.firstinspires.ftc.teamcode.*;
 
-public class MMTeleOp extends OpMode {
+
+public class MMTeleOp{
 
 }
